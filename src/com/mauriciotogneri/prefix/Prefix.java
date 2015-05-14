804c23fe38079f76b6825bfbac4852020c7a07e9
Mauricio Togneri <mauricio.togneri@gmail.com>
2015-05-14 15:04:55 +0200

    Added basic console reader

--- src/com/mauriciotogneri/prefix/Prefix.java
+++ src/com/mauriciotogneri/prefix/Prefix.java
@@ -1,9 +1,44 @@
 package com.mauriciotogneri.prefix;
 
+import java.util.Scanner;
+
 public class Prefix
 {
+	public void run(String filePath)
+	{
+		Program program = new Program();
+		program.parse(filePath);
+		
+		if (program.isValid())
+		{
+			Object result = program.run();
+			System.out.println(result);
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			while (true)
+			{
+				System.out.print("\n> ");
+				String expression = scanner.nextLine();
+				
+				if (!expression.isEmpty())
+				{
+					Object output = program.evaluate(expression);
+					System.out.println(output);
+				}
+				else
+				{
+					break;
+				}
+			}
+			
+			scanner.close();
+		}
+	}
+	
 	public static void main(String[] args)
 	{
-		
+		Prefix prefix = new Prefix();
+		prefix.run(""); // TODO: args[0]
 	}
 }
---
+++ src/com/mauriciotogneri/prefix/Program.java
@@ -0,0 +1,24 @@
+package com.mauriciotogneri.prefix;
+
+public class Program
+{
+	public void parse(String filePath)
+	{
+		
+	}
+	
+	public Object evaluate(String expression)
+	{
+		return null;
+	}
+	
+	public boolean isValid()
+	{
+		return true;
+	}
+	
+	public Object run()
+	{
+		return null;
+	}
+}
